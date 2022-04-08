4c2f46731ed6d56fc5972719d23e079cee4caf71
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-04-08 12:39:28 +0000

    Added Docs for Routes and Validation rules

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,17 +1,20 @@
 <?php
 
 /**
- * ? Allowed origins: *
- * ? Allowed methods: GET, POST, PUT, DELETE
- * ? Allowed headers: clientRef, Content-Type, Authorization, X-Requested-With
- * ? Allowed credentials: true
- * ? Max age: 86400
+ * Routes Definition
+ * 
+ * ? Allowed Headers:
+ * ? - Allowed origins: *
+ * ? - Allowed methods: GET, POST, PUT, DELETE, OPTIONS
+ * ? - Allowed headers: headerRef, Content-Type, Authorization, X-Requested-With
+ * ? - Allowed credentials: true
+ * ? - Max age: 86400
  * -----------------------------------------------------------------------------
  * ? Availlable Middlewares:
  * ? - Auth: check if user is logged in with "JWT" or "API KEY in Header" and has the right permissions to access the route
  * ? - Validation: Sanitize input data and check if it is valid (e.g. email, password, etc.)
  * -----------------------------------------------------------------------------
- * ? - Auth MIDDLEWARE PARAMETERS:
+ * ? Auth MIDDLEWARE PARAMETERS:
  * ? - - guest: check if user is not logged in
  * ? - - user: check if user is logged in
  * ? - - admin: check if user is logged in and has the right permissions to access the route
@@ -27,6 +30,9 @@
  * * $router->put('/', 'HomeController@index');
  * ? - DELETE ROUTE:
  * * $router->delete('/', 'HomeController@index');
- * ? - ROUTE WITH MIDDLEWARE:
- * * $router->get('/', 'HomeController@index', ['Auth@role', Validation@field1|field2|field3...]);
+ * ? - ROUTE WITH MIDDLEWARES:
+ * * $router->get('/', 'HomeController@index', ['Auth@guest', Validation@email|password|etc.']);
+ * 
+ * @package App\Config
+ * @author Mohammed-Aymen Benadra
  */
---
+++ app/config/validation-rules.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\{Example};
+
+/**
+ * Validation Rules
+ * 
+ * * ? Available Validators:
+ * ? - - required: Check if the field is required
+ * ? - - string: Check if the field is a string
+ * ? - - numeric: Check if the field is numeric
+ * ? - - min: Check if the field is greater than or equal to the parameter provided
+ * ? - - max: Check if the field is less than or equal to the parameter provided
+ * ? - - int: Check if the field is an integer
+ * ? - - float: Check if the field is a float
+ * ? - - bool: Check if the field is a boolean
+ * ? - - array: Check if the field is an array
+ * ? - - object: Check if the field is an object
+ * ? - - email: Check if the field is an email
+ * ? - - file: Check if the field is a file
+ * ? - - image: Check if the field is an image
+ * ? - - url: Check if the field is a url
+ * ? - - date: Check if the field is a date
+ * ? - - date_format: Check if the field matches with the parameter provided
+ * ? - - same: Check if the field matches with the parameter provided
+ * ? - - matches: Check if the field matches with the parameter provided
+ * ? - - ip: Check if the field is an ip address
+ * 
+ * @package App\Config
+ * @author Mohammed-Aymen Benadra
+ */
+
+$example = new Example();
+
+return array_merge(
+    $example->getRequiredSchema()
+);
