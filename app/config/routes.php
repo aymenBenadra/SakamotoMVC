289889456be7d72c2d177cdc6ef9e77b2dd6e40d
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-04-08 12:16:30 +0000

    Added Routes Documentation for a better understanding

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,43 +1,32 @@
 <?php
 
-// Pages
-
-
-// Home page
-$router->get('public', 'Pages@index');
-// About page
-$router->get('public/about', 'Pages@about');
-
-// Posts
-
 /**
- * Get Routes
+ * ? Allowed origins: *
+ * ? Allowed methods: GET, POST, PUT, DELETE
+ * ? Allowed headers: clientRef, Content-Type, Authorization, X-Requested-With
+ * ? Allowed credentials: true
+ * ? Max age: 86400
+ * -----------------------------------------------------------------------------
+ * ? Availlable Middlewares:
+ * ? - Auth: check if user is logged in with "JWT" or "API KEY in Header" and has the right permissions to access the route
+ * ? - Validation: Sanitize input data and check if it is valid (e.g. email, password, etc.)
+ * -----------------------------------------------------------------------------
+ * ? - Auth MIDDLEWARE PARAMETERS:
+ * ? - - guest: check if user is not logged in
+ * ? - - user: check if user is logged in
+ * ? - - admin: check if user is logged in and has the right permissions to access the route
+ * ? - Validation MIDDLEWARE PARAMETERS:
+ * ? - - field1|field2|field3: List of fields to check if they are valid (e.g. email, password, etc.)
+ * -----------------------------------------------------------------------------
+ * ? Example:
+ * ? - GET ROUTE:
+ * * $router->get('/', 'HomeController@index');
+ * ? - POST ROUTE:
+ * * $router->post('/', 'HomeController@index');
+ * ? - PUT ROUTE:
+ * * $router->put('/', 'HomeController@index');
+ * ? - DELETE ROUTE:
+ * * $router->delete('/', 'HomeController@index');
+ * ? - ROUTE WITH MIDDLEWARE:
+ * * $router->get('/', 'HomeController@index', ['Auth@role', Validation@field1|field2|field3...]);
  */
-// Go to Posts index page
-$router->get('public/posts', 'Posts@index');
-// Show Post Details
-$router->get('public/post', 'Posts@show');
-// Create new Post page
-$router->get('public/post/create', 'Posts@create');
-// Edit Post page
-$router->get('public/post/edit', 'Posts@edit');
-// Delete Post page
-$router->get('public/post/delete', 'Posts@delete');
-
-/**
- * Post Routes
- */
-// Store new post
-$router->post('public/post/store', 'Posts@store');
-// Update post by ID
-$router->post('public/post/update', 'Posts@update');
-// Delete post by ID
-$router->post('public/post/destroy', 'Posts@destroy');
-
-/**
- * Experimental Routes
- */
-// Update post by ID
-// $router->put('public/post', 'Posts@update');
-// Delete post by ID
-// $router->delete('public/post', 'Posts@destroy');
