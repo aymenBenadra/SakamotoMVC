2cb8274a2542f378e64236a39af8ca4c6d849894
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-04-08 13:50:09 +0000

    Added Example Model for easier scaffolding of app Models

---
+++ app/models/Example.model.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Core\Model;
+
+/**
+ * Example Model
+ * 
+ * @package App\Models
+ * @uses Core\Model Core Model
+ * @author Mohammed-Aymen Benadra
+ */
+class Example extends Model
+{
+    public function __construct()
+    {
+        parent::__construct([
+            'exampleField' => 'required|numeric|min:1|max:10|int|float|bool|array|object|email|file|image|url|date|date_format|same|matches|ip'
+        ]);
+        $this->table = 'Examples';
+    }
+}
